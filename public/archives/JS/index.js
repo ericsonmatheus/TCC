dc67efeb321d7d56e3ca304bb8f83ff886efb898
vitor abreu <vitorabreufjb@hotmail.com>
2021-04-16 17:41:01 -0300

    tela cardapio

--- public/archives/JS/index.js
+++ public/archives/JS/index.js
@@ -1,9 +1,8 @@
 
 var abrir = document.getElementById('abrirMenu')
-
-
-var abrir = document.getElementById('abrirMenu')
 var fechar = document.getElementById('fecharMenu')
+
+
 
 
 abrir.addEventListener('click', function(){
@@ -16,5 +15,21 @@ fechar.addEventListener('click', function(){
     var menu = document.getElementById('menu')
 
     menu.style.width = '0'
+    menu.style.transition = 'width 0.2s ease'
+})
+
+var show = document.getElementById('show')
+var close = document.getElementById('close')
+
+show.addEventListener('click', function(){
+    var adicionarCardapio = document.getElementById('adicionar')
+    adicionarCardapio.style.width = '50%'
+    adicionarCardapio.style.padding = '25px'
+})
+
+close.addEventListener('click', function(){
+    var adicionarCardapio = document.getElementById('adicionar')
+    adicionarCardapio.style.width = '0'
+    adicionarCardapio.style.padding = '0px'
 })
 
