863db7c7b2bb837171e630431d173ae874f40b57
ericson matheus <ericson_matheus.250@hotmail.com>
2021-05-09 20:44:47 -0300

    separando telas de adm

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -28,22 +28,9 @@
     </header>
 
     @if (session()->has('sessionFunc'))
-        <nav id="menu">
-            <a id="fecharMenu"><i class="fas fa-bars"> Fechar</i></a>
-            <a href="{{ route('adm.index')}}"><i class="fas fa-home"></i> Home</a>
-            <a href="{{ route('adm.cardapio') }}"><i class="fas fa-list-alt"></i> Cardapio</a>
-            <a href="{{ route('adm.carteira') }}"><i class="fas fa-wallet"></i> Carteira</a>
-            <a href="{{ route('adm.sobre') }}"><i class="fas fa-exclamation-circle"></i> Sobre</a>
-            <a href="{{ route('adm.contato') }}"><i class="fas fa-tty"></i> Contato</a>
-            <a href="#"><i class="fas fa-tools"></i> Configurações</a>
-            <div class="copyright">&copy; Burguerboss<em id="date"></em>. Todos os direitos reservados.</div>
-        </nav>
+        @include('viewSelect.logado')
     @else
-    <nav id="menu">
-        <a id="fecharMenu"><i class="fas fa-bars"> Fechar</i></a>
-        <a href="{{ route('adm.index')}}"><i class="fas fa-home"></i> Home</a>
-        <div class="copyright">&copy; Burguerboss<em id="date"></em>. Todos os direitos reservados.</div>
-    </nav>
+        @include('viewSelect.naoLogado')
     @endif
     
 
