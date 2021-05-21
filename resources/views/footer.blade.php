4475861f2d274663e8710983f5af7169573e1b6b
vitororigens <vitorabreufjb@hotmail.com>
2021-05-20 21:04:45 -0300

    ajsute no carrinho

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,6 +5,5 @@
     integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
 </script>
 <script src="{{ mix('js/script.js') }}"></script>
-<script src="https://igorescobar.github.io/jQuery-Mask-Plugin/js/jquery.mask.min.js"></script>  
 
 </html>
